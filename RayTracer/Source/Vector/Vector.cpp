48423be856cc884e910fc5d418067b02dc5f4b27
fdcumt <fdcumt@gmail.com>
2022-04-06 11:11:13 +0800

    添加Ray逻辑

---
+++ RayTracer/Source/Vector/Vector.cpp
@@ -0,0 +1,18 @@
+#include "Vector.h"
+
+
+const FVector FVector::ZeroVector(0.f, 0.f, 0.f);
+
+const FVector FVector::OneVector(1.f, 1.f, 1.f);
+
+const FVector FVector::UpVector(0.f, 0.f, 1.f);
+
+const FVector FVector::DownVector(0.f, 0.f, -1.f);
+
+const FVector FVector::ForwardVector(1.f, 0.f, 0.f);
+
+const FVector FVector::BackwardVector(-1.f, 0.f, 0.f);
+
+const FVector FVector::RightVector(0.f, 1.f, 0.f);
+
+const FVector FVector::LeftVector(0.f, -1.f, 0.f);
